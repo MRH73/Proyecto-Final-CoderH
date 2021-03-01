fb1116b99219b745954ad4b7e7660dc3cc65e20c
Miguel Ruiz <miruhe1995@gmail.com>
2021-03-01 11:12:03 -0500

    Repositorio 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let btnMenuContainer = document.getElementById("btnMenuContainer");
+let menu = document.getElementById("menu");
+
+btnMenuContainer.addEventListener("click", function(){
+    menu.classList.toggle("visible");
+})
+
+ScrollReveal().reveal('.imgS3', { delay: 500 });
+ScrollReveal().reveal('.contenedorServicio3', { delay: 500 });
+ScrollReveal().reveal('.imgS2', { delay: 500 });
+ScrollReveal().reveal('.contenedorServicio2', { delay: 500 });
+
+ScrollReveal().reveal('.imgC1', { delay: 500 });
+ScrollReveal().reveal('.imgC2', { delay: 500 });
+ScrollReveal().reveal('.imgC3', { delay: 500 });
+ScrollReveal().reveal('.contenedorComputador1', { delay: 500 });
+ScrollReveal().reveal('.contenedorComputador2', { delay: 500 });
+ScrollReveal().reveal('.contenedorComputador3', { delay: 500 });
+
+ScrollReveal().reveal('.contenedorGridPYA', { delay: 500 });
+ScrollReveal().reveal('.contenedorGridPYA2', { delay: 500 });
